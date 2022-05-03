e532549ed75b7a57bdeecb85b485e00eb443d0f1
Jimmy Andreas <jimmysignature@gmail.com>
2022-05-02 20:46:11 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const sumBtn = document.querySelector("#create-task-form > input:last-child")
+  // console.log(sumBtn)
+// const btn = document.querySelector("#btn")
+
+
+sumBtn.addEventListener("click", function (e){
+    e.preventDefault()
+    console.log(e)
+    //What do we want to add to the DOM
+const listItem = document.createElement("li")
+  //Where do we want the todo to be added
+const task = document.querySelector("#tasks")
+  //Set the value to the input box
+  const newTask = document.querySelector("#new-task-description").value
+  console.log(newTask)
+  listItem.textContent = newTask
+  //Add the item to the DOM
+ task.appendChild(listItem)
+ })
+    
+  
 });
